7fe92388ffcdc6cdd9ad82f61a25f21cbca026cd
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-02 23:45:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,49 +1,33 @@
 // Code your solution in this file!
 
-
-function distanceFromHqInBlocks(block){
-  if(block>42)
-  {
-  return block -  42;
-
-  }else{
-    return 42 - block;
+function distanceFromHqInBlocks (blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
+  } else {
+    return 42 - blockNumber;
   }
-  
-  }
-  
-  
-  
+}
+
 function distanceFromHqInFeet (blockNumber) {
-    if(blockNumber>42)
-  {
-  return (blockNumber -  42) * 264;
+  return distanceFromHqInBlocks(blockNumber) * 264;
+}
 
-  }else{
-    return (42 - blockNumber) * 264;
+function distanceTravelledInFeet (start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
   }
 }
 
-function distanceTravelledInFeet(a ,b){
-    if(b>=a)
-  {
-  return (b-a) * 264;
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
 
-  }else{
-    return (a-b) * 264;
-  }
-}
-
-
-function calculatesFarePrice(a,b){
-  let c=distanceTravelledInFeet(a ,b);
-  
-  if(c <=400){
+  if (distance <= 400) {
     return 0;
-  }
-  else if(c > 400 && c <= 2000){
-    return .02 * c;
-  }else if (c > 2000 && c < 2500) {
+  } else if (distance > 400 && distance <= 2000) {
+    return   (distance -400)*0.02;
+  } else if (distance > 2000 && distance < 2500) {
     return 25;
   } else {
     return 'cannot travel that far';
